7c00c53b322c8eed2d7e727b48eadb2edf820a24
vluo1 <vluo@wgu.edu>
2024-08-28 00:14:13 -0400

    Add navbar colour

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
 
     <header style={{ textAlign: 'center' }} className="bg-blue-100 ">
       <div className="flex items-center"> 
-      <img src="/images/SmallLogo.png" style={{width: '80px', height: '55px'}}  alt="logo" className="w-20 h-auto mr-4"/>
+      <img src="/images/SmallLogo.png" style={{width: '120px', height: '83px'}}  alt="logo" className="w-20 h-auto mr-4"/>
       </div>
       
       <nav className="flex header-nav flex-row">
