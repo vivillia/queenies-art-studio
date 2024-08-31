7bf90524ed3e3e6d1f56ddd1170ff2001132e5a0
vluo1 <vluo@wgu.edu>
2024-08-31 05:05:42 -0400

    Add logo component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 import './App.css'; // Main CSS file
 import Header from './components/header'; // Import the Header component
 import Announcement from './components/announcement';
+import Logo from './components/logo';
 
 const App: React.FC = () => {
   const items = ['Text 1', 'Text 2', 'Text 3'];
   return (
     <div className="App">
-      <Header />
-
+      
       <main>
+        <Logo />
          <Announcement items={items} />
+         <Header />
       </main>
     </div>
   );
---
+++ src/components/logo.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./logo.css";
+
+const Logo: React.FC = () => {
+  return (
+
+<header style={{ textAlign: 'center' }} className="white">
+      <div className="flex items-center"> 
+       <img src="/images/SmallLogo.png" style={{width: '120px', height: '83px'}}  alt="logo" className="w-20 h-auto mr-4"/>
+      </div>
+</ header>
+
+  );
+};
+
+export default Logo;
