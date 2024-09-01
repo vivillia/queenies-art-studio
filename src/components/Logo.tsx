cc11e7ad302ec820a685c183fab06de3a4ec1ad3
vluo1 <vluo@wgu.edu>
2024-09-01 18:21:11 -0400

    Update Logo to link to homepage

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,9 @@ const Logo: React.FC = () => {
 
 <header style={{ textAlign: 'center' }} className="white">
       <div className="flex items-center"> 
-       <img src="/images/SmallLogo.png" style={{width: '120px', height: '83px'}}  alt="logo" className="w-20 h-auto mr-4"/>
+        <a href="/">
+          <img src="/images/SmallLogo.png" style={{width: '120px', height: '83px'}}  alt="logo" className="w-20 h-auto mr-4"/>
+       </a>
       </div>
 </ header>
 
