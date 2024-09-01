157377b764bc5c81c50258a3c3242e726f56ef15
vluo1 <vluo@wgu.edu>
2024-09-01 19:31:22 -0400

    Add React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,21 @@
-// App.tsx
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css'; // Main CSS file
-import Announcement from './components/Announcement';
-import Logo from './components/Logo';
-import PhotoCarousel from './components/PhotoCarousel';
+import Home from './pages/home';
+import Gallery from './pages/Gallery';
 
 const App: React.FC = () => {
-  const items = [
-    'Welcome to Queenie\'s Art Studio!', 
-    'It\'s back to school season!', 
-    'This website is currently a work in progress, stay tuned for more!'
-  ];
-
-  const interval = 8888; // Interval in milliseconds 
-
-  const images = [
-    { src: "/images/ExpertArtwork1.png" },
-    { src: "/images/JuniorArtwork1.png" },
-    { src: "/images/JuniorArtwork3.png" }
-  ];
-
-
   return (
-    <div className="App">
-      
-      <main>
-        <Logo />
-        <Announcement items={items} interval={interval} />
-        <PhotoCarousel images={images} interval={interval} />
-
-      </main>
-    </div>
+    <Router>
+      <div className="App">
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery" element={<Gallery />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 };
 
